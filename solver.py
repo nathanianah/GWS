2442a0e3934938210562c20438f244d98f664049
nathanianah <nathanianah@gmail.com>
2022-02-16 09:40:43 -0500

    Added simple solver
    Randomly selects from a list of filtered words based on clues

--- solver.py
+++ solver.py
@@ -21,29 +21,75 @@ def check_guess(guess, solution):
 			result += 'X'
 	return result
 
+class Guesser:
+	def __init__(self, allowed):
+		self.words = [x for x in allowed]
+
+	def possible(self, clue, last_guess, word):
+		check = True
+		for i,x in enumerate(clue):
+			if x=='G':
+				if word[i] != last_guess[i]:
+					check = False
+			elif x == 'Y':
+				if last_guess[i] not in word or word[i]==last_guess[i]:
+					check = False
+			elif x == 'X':
+				if last_guess[i] in word:
+					check = False
+		return check
+
+	def make_guess(self, clue, last_guess):
+		if not clue:
+			return random.choice(self.words)
+		self.words = [word for word in self.words if self.possible(clue, last_guess, word)]
+		return random.choice(self.words)
+
 #print(choose_solution())
+def make_attempt(clue, tried):
+	if not clue:
+		candidate = [x for x in allowed]
+		return random.choice(candidate)
+	return random.choice(possible)
+
 
 def play_game():
 	answer = choose_solution()
 	# attempt = input("Enter guess: ")
 	# print(attempt)
 	guesses = []
+	ai = Guesser(allowed)
+	clue = ""
+	attempt = ""
 	for x in range(6):
-		attempt = ""
-		while attempt in guesses or attempt not in allowed:
-			attempt = input("Enter guess: ")
-		print(attempt, "accepted")
+		# attempt = ""
+		# while attempt in guesses or attempt not in allowed:
+		# 	attempt = input("Enter guess: ")
+		# print(attempt, "accepted")
+		attempt = ai.make_guess(clue, attempt)
 		guesses += [attempt]
 
-		print(guesses)
+		# print(guesses)
 		result = check_guess(attempt, answer)
-		print (result)
+		clue = result
+		# print (result)
 
 		if attempt == answer:
-			print("Correct")
+			# print("Correct")
+			return True, len(guesses)
 			break
-		else:
-			print("Try again")
-	print("Solution", answer)
+		# else:
+			# print("Try again")
+	# print("Solution", answer)
+	return False, 0
 
-play_game()
+correct = 0
+total_tries = 0
+for x in range(100):
+	win, tries = play_game()
+	if win:
+		correct += 1
+		total_tries += tries
+
+print(correct,"guessed out of 100")
+print(total_tries/correct,'average guesses')
